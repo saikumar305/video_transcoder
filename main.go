547582272fd80759e0542f152c7edb9095cdfcb3
saikumar305 <saikumarrachakonda305@gmail.com>
2025-10-06 22:36:56 +0530

    init

--- main.go
+++ main.go
@@ -2,13 +2,44 @@ package main
 
 import (
 	"fmt"
-	"video_transcoder/config"
 )
 
 
-func main(){
-	fmt.Println("Hello")
 
-	db := config.DatabaseConnection()
-	fmt.Println(db)
+var name string = "sai"
+
+func add(a int , b int) int {
+		return a+b
+	}
+
+
+func main() {
+	age := 7
+	fmt.Println("Hello, World!" , name)
+
+	if age > 18 {
+		fmt.Println("adult")
+	}else{
+		fmt.Println("minor")
+	}
+
+	// loops
+
+	for i := 0; i<5; i++ {
+		fmt.Println(i*i)
+	}
+
+	
+
+	fmt.Println(add(1,3))
+
+	x := 10
+    p := &x // p is a pointer to x
+
+    fmt.Println("Value of x:", x)
+    fmt.Println("Address of x:", &x)
+    fmt.Println("Value stored in p (address):", p)
+    fmt.Println("Value pointed by p:", *p)
 }
+
+
